6fe380e84e5da3d35a4b869d1e759f61a52dff3a
Jeff Wang <jeffawang@gmail.com>
2021-11-22 22:27:44 -0800

    basic syncing yjs impl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+import * as Y from 'yjs'
+
 import Draggable from 'react-draggable'
+import { WebrtcProvider } from 'y-webrtc';
 
 const fridgeStyle = {
   position: 'relative',
-  height: '300px',
+  height: '512px',
+  width: '512px',
   backgroundColor: '#ddd',
   boxShadow: 'inset 0px 0px 10px 3px #999'
 }
@@ -19,24 +25,144 @@ const magnetStyle = {
   borderBottom: '3px solid black',
 }
 
-const Magnet = ({left, top, word}) => {
-  return <Draggable bounds="parent" defaultPosition={{x: left, y: top}}>
-    <div style={{...magnetStyle}}>{word}</div>
+const Magnet = ({word}) => {
+  const data = word.toJSON()
+
+  const onDrag = (e, drag) => {
+    const newPos = {
+      x: drag.x,
+      y: drag.y,
+    }
+    word.set("position", newPos)
+  }
+
+  const [xy, setXY] = useState(data.position)
+
+  useEffect(()=>{
+    word.observe((e)=> {
+      setXY(e.target.get("position"))
+    }
+    )
+  }, [])
+
+  return <Draggable bounds="parent" position={data.position} onDrag={onDrag}>
+    <div style={{...magnetStyle}}>{data.word}</div>
   </Draggable>
 }
 
+const defaultWords = [
+  {
+    "word": "hello",
+    "position": {
+      "x": 10,
+      "y": 20
+    }
+  },
+  {
+    "word": "world",
+    "position": {
+      "x": 100,
+      "y": 66
+    }
+  },
+  {
+    "word": "(",
+    "position": {
+      "x": 50,
+      "y": 200
+    }
+  },
+  {
+    "word": ")",
+    "position": {
+      "x": 80,
+      "y": 160
+    }
+  },
+  {
+    "word": "print",
+    "position": {
+      "x": 120,
+      "y": 30
+    }
+  },
+  {
+    "word": '"',
+    "position": {
+      "x": 10,
+      "y": 100
+    }
+  },
+  {
+    "word": '"',
+    "position": {
+      "x": 100,
+      "y": 200
+    }
+  }
+]
+
 function App() {
+
+  const [words, setWords] = useState([])
+  const [provider, setProvider] = useState()
+  const [awareness, setAwareness] = useState()
+
+  const [yarrayLog, setYArrayLog] = useState([])
+
+  useEffect(() => {
+    const ydoc = new Y.Doc()
+    const p = new WebrtcProvider('fridge-magnets-12345', ydoc)
+
+    const yarray = ydoc.getArray('words')
+
+    setWords(yarray)
+
+    yarray.observeDeep((event) => {
+      setYArrayLog(yarrayLog.concat([event]))
+    })
+
+
+    setProvider(p)
+    setAwareness(p.awareness)
+
+  }, [])
+
+  const [newWord, setNewWord] = useState("hello")
+
+  const onNewWordInputChange = (e) => {
+    const value = e.target.value
+    setNewWord(value)
+  }
+
+  const onNewWordSubmit = () => {
+    if (newWord.length === 0) return
+    const ymap = new Y.Map(Object.entries({...defaultWords[0], word: newWord}))
+    // ymap.set('word', newWord)
+    // ymap.set('position', {x: 10, y: 25})
+    words.push([ymap])
+    setNewWord("")
+  }
+
   return (
-    <div style={{padding: "30px"}}>
-      <div style={fridgeStyle}>
-        <Magnet left={10} top={20} word="hello" />
-        <Magnet left={100} top={66} word="world" />
-        <Magnet left={50} top={200} word="(" />
-        <Magnet left={80} top={160} word=")" />
-        <Magnet left={120} top={30} word="print" />
-        <Magnet left={10} top={100} word='"' />
-        <Magnet left={100} top={200} word='"' />
+    <div>
+      <div style={{padding: "30px"}}>
+        <div style={fridgeStyle}>
+          {
+            words.map((word) => {
+              return <Magnet word={word} />
+            })
+          }
+        </div>
       </div>
+      <div>
+        <p>New Word</p>
+        <input value={newWord} onChange={onNewWordInputChange}/>
+        <button onClick={onNewWordSubmit}>submit new word</button>
+      </div>
+      <p>WORDS: {JSON.stringify(words)}</p>
+      {/* <p>DRAG LOG: {JSON.stringify(dragLog, getCircularReplacer())}</p> */}
+      {/* <p>YARRAY LOG: {JSON.stringify(yarrayLog)}</p> */}
     </div>
   );
 }
